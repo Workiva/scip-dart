01ae41c16c32cd8d0eecb8dac527c28aa2473ab6
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-03-01 17:41:04 -0700

    revert fmt of snap and gen files

--- snapshots/output/basic-project/lib/other.dart
+++ snapshots/output/basic-project/lib/other.dart
@@ -1,13 +1,14 @@
-class Foo {
+  class Foo {
 // definition scip-dart pub dart_test 1.0.0 lib/other.dart/
 //      ^^^ definition scip-dart pub dart_test 1.0.0 lib/other.dart/Foo#
 //      documentation ```dart
-  int _far;
+    int _far;
 //  ^^^ reference scip-dart pub dart:core 2.18.0 lib/core/int.dart/int#
 //      ^^^^ definition local 0
 //      documentation ```dart
-  Foo(this._far);
+    Foo(this._far);
 //  ^^^ definition scip-dart pub dart_test 1.0.0 lib/other.dart/Foo#<constructor>().
 //  documentation ```dart
 //  ^^^ reference scip-dart pub dart_test 1.0.0 lib/other.dart/Foo#
-}
+  }
+  
--- tool/dart_dev/config.dart
+++ tool/dart_dev/config.dart
@@ -1,3 +1,10 @@
 import 'package:dart_dev/dart_dev.dart';
+import 'package:glob/glob.dart';
 
-final config = coreConfig;
+final config = {
+  ...coreConfig,
+  'format': FormatTool()..exclude = [
+    Glob('snapshots/**'),
+    Glob('lib/src/gen/*.dart')
+  ]
+};
