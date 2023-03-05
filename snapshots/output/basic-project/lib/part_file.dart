88890a3afdde69f930094b81ff8269856095b89c
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-03-05 16:03:58 -0700

    regen snaps

--- snapshots/output/basic-project/lib/part_file.dart
+++ snapshots/output/basic-project/lib/part_file.dart
@@ -1,2 +1,3 @@
   part of 'directives.dart';
 // definition scip-dart pub dart_test 1.0.0 lib/part_file.dart/
+//        ^^^^^^^^^^^^^^^^^ reference scip-dart pub dart_test 1.0.0 lib/directives.dart/
