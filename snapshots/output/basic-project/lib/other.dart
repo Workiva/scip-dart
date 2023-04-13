88f559ea13b2858980a3b6a96d5e68414054fdfb
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-04-13 09:54:46 -0600

    added field accessors

--- snapshots/output/basic-project/lib/other.dart
+++ snapshots/output/basic-project/lib/other.dart
@@ -15,3 +15,28 @@
 //           documentation ```dart
   }
   
+  class Bar {
+//      ^^^ definition scip-dart pub dart_test 1.0.0 lib/other.dart/Bar#
+//      documentation ```dart
+    String _someValue;
+//  ^^^^^^ reference scip-dart pub dart:core 2.18.0 dart:core/string.dart/String#
+//         ^^^^^^^^^^ definition local 2
+//         documentation ```dart
+    Bar(this._someValue);
+//  ^^^ definition scip-dart pub dart_test 1.0.0 lib/other.dart/Bar#<constructor>().
+//  documentation ```dart
+//  ^^^ reference scip-dart pub dart_test 1.0.0 lib/other.dart/Bar#
+//      ^^^^ reference local 2
+//           ^^^^^^^^^^ definition local 3
+//           documentation ```dart
+  
+    void someMethod() {
+//       ^^^^^^^^^^ definition scip-dart pub dart_test 1.0.0 lib/other.dart/Bar#someMethod().
+//       documentation ```dart
+      _someValue = 'asdf';
+//    ^^^^^^^^^^ reference local 2
+      print(_someValue);
+//    ^^^^^ reference scip-dart pub dart:core 2.18.0 dart:core/print.dart/print().
+//          ^^^^^^^^^^ reference local 2
+    }
+  }
