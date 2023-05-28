da55b6bcfa0c42ff64eb3ec4ebc0bd8235655e0d
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-05-28 16:35:08 -0600

    cleaned up implementation to be specific to classes

--- lib/src/relationship_generator.dart
+++ lib/src/relationship_generator.dart
@@ -9,6 +9,9 @@ List<Relationship>? relationshipsFor(
   Element element,
   SymbolGenerator symbolGenerator,
 ) {
+  // Only classes have relationships. Mixins and enums do not support
+  // inheritance themselves and therefore do not have any relationships
+  // to define.
   if (element is ClassElement) {
     // All things inherit from Object, if they have one supertype
     // there are no relationships
@@ -17,25 +20,28 @@ List<Relationship>? relationshipsFor(
     return element.allSupertypes
         .where((type) => !type.isDartCoreObject)
         .map((type) => Relationship(
-          symbol: symbolGenerator.symbolFor(type.element),
-          isImplementation: true,
-        ))
+              symbol: symbolGenerator.symbolFor(type.element),
+              isImplementation: true,
+            ))
         .toList();
   }
 
-  if (node is MethodDeclaration) {
-    final parentNode = node.parent as NamedCompilationUnitMember;
-    final parentElement = parentNode.declaredElement as InterfaceElement;
+  // Since mixins do not support inheritance, we only care about
+  // methods that exist on classes
+  if (node is MethodDeclaration && node.parent is ClassDeclaration) {
+    final parentNode = node.parent as ClassDeclaration;
+    final parentElement = parentNode.declaredElement as ClassElement;
 
     final referencingTypes = parentElement.allSupertypes
-      .where(_interfaceContainsName(node.name.toString()));
+        .where(_interfaceContainsName(node.name.toString()));
 
     if (referencingTypes.isNotEmpty) {
-      return referencingTypes.map((type) => Relationship(
-        symbol: symbolGenerator.symbolFor(type.element),
-        isImplementation: true,
-        isReference: true
-      )).toList();
+      return referencingTypes
+          .map((type) => Relationship(
+              symbol: symbolGenerator.symbolFor(type.element),
+              isImplementation: true,
+              isReference: true))
+          .toList();
     }
   }
 
@@ -44,9 +50,8 @@ List<Relationship>? relationshipsFor(
 
 bool Function(InterfaceType) _interfaceContainsName(String name) {
   return (InterfaceType interfaceType) {
-    return [
-      ...interfaceType.methods,
-      ...interfaceType.accessors
-    ].map((ref) => ref.name).contains(name);
+    return [...interfaceType.methods, ...interfaceType.accessors]
+        .map((ref) => ref.name)
+        .contains(name);
   };
 }
