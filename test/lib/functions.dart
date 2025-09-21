21f011c530db1424a5224a05c36b88a8d670586c
Matthew Nitschke <matthew.nitschke@workiva.com>
2025-09-21 16:26:18 -0600

    fleshed out parameters

--- test/lib/functions.dart
+++ test/lib/functions.dart
@@ -1,10 +1,12 @@
 
 void main() {
   withReturn();
-  // <- definition asdf
+  // <- reference scip-dart pub scip_dart_test . lib/`functions.dart`/withReturn().
 }
 
-@Deprecated('a')
+void basicFunction() {}
+//   ^^^^^^^^^^^^^ definition scip-dart pub scip_dart_test . lib/`functions.dart`/basicFunction().
+
 String withReturn() => 'asdf';
 // <- reference scip-dart pub dart:core . dart:core/`string.dart`/String#
 
@@ -13,17 +15,34 @@ void withParameters(
 //^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
 //       ^^^ definition local 0
   int pos2,
-//^^ reference scip-dart pub dart:core . dart:core/`int.dart`/int#
+//^^^ reference scip-dart pub dart:core . dart:core/`int.dart`/int#
 //    ^^^^ definition local 1
 ) {}
 
-void withNamedParameters({
+void withNamedParameters(
+  String base, {
+//^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
+//       ^^^^ definition local .
+    
   String? name,
 //^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
 //        ^^^^ definition scip-dart pub scip_dart_test 1.0.0 lib/`functions.dart`/withNamedParameters().(name)
+
   int? name2,
 //^^^ reference scip-dart pub dart:core . dart:core/`int.dart`/int#
 //     ^^^^^ definition scip-dart pub scip_dart_test 1.0.0 lib/`functions.dart`/withNamedParameters().(name2)
 }) {}
 
-void withPositionalParameters(String base, [String? pos, int? pos2]) {}
+void withPositionalParameters(
+  String base, [
+//^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
+//       ^^^^ definition local .
+
+  String? pos,
+//^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
+//        ^^^ definition local .
+
+  int? pos2
+//^^^ reference scip-dart pub dart:core . dart:core/`int.dart`/int#
+//     ^^^^ definition local .
+]) {}
