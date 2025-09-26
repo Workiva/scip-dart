846f9b110893108213efbb926900b8f43051a426
Matthew Nitschke <matthew.nitschke@workiva.com>
2025-09-26 12:53:17 -0600

    added additional test coverage

--- test/lib/functions.dart
+++ test/lib/functions.dart
@@ -26,11 +26,11 @@ void withNamedParameters(
     
   String? name,
 //^^^^^^ reference scip-dart pub dart:core . dart:core/`string.dart`/String#
-//        ^^^^ definition scip-dart pub scip_dart_test 1.0.0 lib/`functions.dart`/withNamedParameters().(name)
+//        ^^^^ definition scip-dart pub scip_dart_test . lib/`functions.dart`/withNamedParameters().(name)
 
   int? name2,
 //^^^ reference scip-dart pub dart:core . dart:core/`int.dart`/int#
-//     ^^^^^ definition scip-dart pub scip_dart_test 1.0.0 lib/`functions.dart`/withNamedParameters().(name2)
+//     ^^^^^ definition scip-dart pub scip_dart_test . lib/`functions.dart`/withNamedParameters().(name2)
 }) {}
 
 void withPositionalParameters(
